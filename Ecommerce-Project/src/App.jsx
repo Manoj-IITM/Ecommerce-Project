37799c2b4f02456d0386caa99f1194bd24960d68
Manoj-IITM <manojiitmsuppp1608@gmail.com>
2025-12-31 23:53:16 +0530

    Added Multiple pages Using React Routing ( react-router)

--- Ecommerce-Project/src/App.jsx
+++ Ecommerce-Project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Routes,Route } from 'react-router';
 import { HomePage } from './pages/HomePage' 
 
 import './App.css'
@@ -7,9 +7,10 @@ function App() {
   
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>Hello</div>} />
+    </Routes>
   )
 }
 
