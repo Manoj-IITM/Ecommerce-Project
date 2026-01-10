5ddbdb9c8fe1fb6a1834521ea944e94ca97411b5
Manoj-IITM <manojiitmsuppp1608@gmail.com>
2026-01-10 13:42:38 +0530

    used beforeEach to remove duplicaton

--- Ecommerce-Project/src/pages/home/Product.test.jsx
+++ Ecommerce-Project/src/pages/home/Product.test.jsx
@@ -1,17 +1,20 @@
-import { it,expect,describe,vi } from 'vitest'
+import { it,expect,describe,vi,beforeEach } from 'vitest'
 import { render, screen} from '@testing-library/react';
 import { Product } from './Product'
 import axios from 'axios';
 import userEvent from '@testing-library/user-event';
 
 
-vi.mock('axios');
+vi.mock('axios');  // to fake the axios 
 
 
 
 describe('Product component',() => {
 
-    const product = {
+    let product;
+    let loadCart;
+    beforeEach(() => {
+        product = {
                 id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
                 image: "images/products/athletic-cotton-socks-6-pairs.jpg",
                 name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -21,9 +24,12 @@ describe('Product component',() => {
                 },
                 priceCents: 1090,
                 keywords: ["socks", "sports", "apparel"]
-            }
+        };
 
-    const loadCart = vi.fn();
+        loadCart = vi.fn();
+        
+    })
+
 
     it('displays product correctly',() => {
         render(<Product  product={product} loadCart={loadCart}/>)
@@ -46,19 +52,6 @@ describe('Product component',() => {
 
     it('click on add to cart', async () => {
 
-        const product = {
-                id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-                name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-                rating: {
-                stars: 4.5,
-                count: 87
-                },
-                priceCents: 1090,
-                keywords: ["socks", "sports", "apparel"]
-            }
-
-        const loadCart = vi.fn();
         render(<Product  product={product} loadCart={loadCart}/>)
 
         const user = userEvent.setup();
